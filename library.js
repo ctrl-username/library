8eb028227e408d77ceb25126683f490e9d29c9cd
ctrl-username <243785975+ctrl-username@users.noreply.github.com>
2026-03-14 18:28:23 +0100

    add book constructor

--- library.js
+++ library.js
@@ -1,9 +1,17 @@
 const myLibrary = [];
 
-function Book(){
+function Book(title, author, pages, read) {
+  if (!new.target) {
+    throw new Error("Book must be instantiated with 'new'");
+  }
   //
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function () {
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? "read" : "not read"}`;
+  };
 }
 
-function addBookToLibrary(){
-  
-}
+function addBookToLibrary() {}
