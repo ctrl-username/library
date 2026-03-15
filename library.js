bb6136f36537377eb2409432b1771dfe30f41e97
ctrl-username <243785975+ctrl-username@users.noreply.github.com>
2026-03-15 09:15:54 +0100

    use prototype for info method, add unique id via crypto.randomUUID

--- library.js
+++ library.js
@@ -5,13 +5,18 @@ function Book(title, author, pages, read) {
     throw new Error("Book must be instantiated with 'new'");
   }
   //
+  this.id = crypto.randomUUID();
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
-  this.info = function () {
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? "read" : "not read"}`;
-  };
 }
 
+Book.prototype.info = function () {
+  return `book id: ${this.id} ${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? "read" : "not read"}`;
+};
 function addBookToLibrary() {}
+
+book1 = new Book("The pragmatic Programmer", "Andy hunt", "55", true);
+
+console.log(book1.info());
