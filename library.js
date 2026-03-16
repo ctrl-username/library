d7362e887a746645213b7e69ebbdba34ab44cdbb
ctrl-username <243785975+ctrl-username@users.noreply.github.com>
2026-03-16 15:55:30 +0100

    add dynamic book card creation, add bottom margin to book-section
    - delete hard coded book section html
    - add create book card function

--- library.js
+++ library.js
@@ -26,10 +26,97 @@ function addBookToLibrary(title, author, pages, read) {
 addBookToLibrary(
   "The pragmatic Programmer",
   "Dave Thomas, Andy hunt",
-  "350",
-  true,
+  350,
+  false,
 );
 
-addBookToLibrary("Clean Code", "Robert Martin", 431, true);
+addBookToLibrary("Clean Code", "Robert Martin", 431, false);
+
+myLibrary[0].toggleRead();
+myLibrary[1].toggleRead();
 myLibrary[0].toggleRead();
 console.log(myLibrary);
+
+function createBookCard(book) {
+  // book
+  const bookDiv = document.createElement("div");
+  bookDiv.className = "book";
+
+  //book cover img
+  const image = document.createElement("img");
+  image.src = book.image || "book_cover.png";
+  image.alt = "book img";
+  // book meta div
+
+  const bookMeta = document.createElement("div");
+  bookMeta.className = "book-meta";
+
+  // Title h3 tag
+  const bookTitle = document.createElement("h3");
+  bookTitle.className = "book-title";
+  bookTitle.textContent = book.title;
+
+  // book author
+  const bookAuthor = document.createElement("div");
+  bookAuthor.className = "book-author";
+  bookAuthor.textContent = book.author;
+
+  //book-pages
+  const bookPages = document.createElement("div");
+  bookPages.className = "book-pages";
+  bookPages.textContent = `${book.pages} pages`;
+
+  // book buttons
+
+  const bookButtons = document.createElement("div");
+  bookButtons.className = "book-buttons";
+
+  // delete button
+  const deleteButton = document.createElement("button");
+  deleteButton.className = "delete-button";
+  deleteButton.textContent = "Delete";
+
+  // mark as read button
+
+  const markAsReadButton = document.createElement("button");
+  markAsReadButton.id = "mark-as-read";
+  markAsReadButton.textContent = `${book.read ? "Mark as read" : "Un-mark as read"}`;
+
+  // share button
+
+  const shareButton = document.createElement("button");
+  shareButton.textContent = "Share";
+
+  //arrange elements
+  //arrange buttons
+  bookButtons.appendChild(deleteButton);
+  bookButtons.appendChild(markAsReadButton);
+  bookButtons.appendChild(shareButton);
+
+  //arrange meta
+  bookMeta.appendChild(bookTitle);
+  bookMeta.appendChild(bookAuthor);
+  bookMeta.appendChild(bookPages);
+  bookMeta.appendChild(bookButtons);
+
+  //arrange book card
+
+  bookDiv.appendChild(image);
+  bookDiv.appendChild(bookMeta);
+
+  return bookDiv;
+}
+
+//display
+function displayLibrary(index) {
+  libraryContainer = document.getElementById("library-container");
+
+  const bookSection = document.createElement("div");
+  bookSection.className = "book-section";
+
+  bookSection.appendChild(createBookCard(myLibrary[index]));
+  // bookSection.appendChild(createBookCard(myLibrary[0]));
+  libraryContainer.appendChild(bookSection);
+}
+displayLibrary(1);
+displayLibrary(0);
